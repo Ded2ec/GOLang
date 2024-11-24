cafa4674f36c1b9f2b61e38b379545a073e5fd00
Nontachai <48468798+Ded2ec@users.noreply.github.com>
2024-11-24 13:04:43 +0900

    first add model

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"backend/internal/models"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
@@ -31,4 +33,55 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) About(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "About, Nontachai")
+}
+
+// Function All name movie
+
+func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
+
+	// create movie
+	var movies []models.Movie
+
+	// set yyyy-mm-dd
+	rd, _ := time.Parse("1993-12-25", "1981-06-12")
+
+	//Data Movie 1
+	yellowboy := models.Movie{
+		ID:          1,
+		Title:       "Yellowboy",
+		ReleaseDate: rd,
+		MPAARating:  "20+",
+		RunTime:     116,
+		Description: "A very nice movie",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+	// Data Movie  slice
+	movies = append(movies, yellowboy)
+
+	rd, _ = time.Parse("2006-01-02", "1982-06-07")
+
+	//Data Movie 2
+	swingingboy := models.Movie{
+		ID:          2,
+		Title:       "Swingingboy",
+		ReleaseDate: rd,
+		MPAARating:  "20+",
+		RunTime:     116,
+		Description: "A very nice movie",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+	// Data Movie  slice
+	movies = append(movies, swingingboy)
+
+	out, err := json.Marshal(movies)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
+
 }
