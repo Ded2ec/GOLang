52b35ef89b2d22ac22766341db43ecbdf8cf08c5
Nontachai <48468798+Ded2ec@users.noreply.github.com>
2024-11-30 15:04:59 +0900

    Install jwt add Token

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 // Set Port
@@ -15,9 +16,15 @@ const port = 8080
 // Appilication Struct&config Database connection
 
 type application struct {
-	DSN    string
-	Domain string
-	DB     repository.DatabaseRepo
+	DSN          string
+	Domain       string
+	DB           repository.DatabaseRepo
+	auth         Auth
+	JWTSecret    string
+	JWTIssuer    string
+	JWTAudience  string
+	CookieDomain string
+	APIKey       string
 }
 
 func main() {
@@ -28,6 +35,15 @@ func main() {
 
 	// Read from command line agements
 	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 dbname=gosampledb user=postgres password=15229 sslmode=disable timezone=UTC connect_timeout=5", "Postgres connect db")
+
+	// Parse the command line arguments for JWT
+	flag.StringVar(&app.JWTSecret, "jwt-secret", "newty_secret", "signing secret")
+	flag.StringVar(&app.JWTIssuer, "jwt-issuer", "example.com", "signing issuer")
+	flag.StringVar(&app.JWTAudience, "jwt-audience", "example.com", "signing audience")
+	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
+	flag.StringVar(&app.Domain, "domain", "example.com", "domain")
+	flag.StringVar(&app.APIKey, "api-key", "b41447e6319d1cd467306735632ba733", "api key")
+
 	flag.Parse()
 
 	// Connect to Database
@@ -38,6 +54,17 @@ func main() {
 	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
 
 	defer app.DB.Conconnection().Close()
+
+	app.auth = Auth{
+		Issuer:        app.JWTIssuer,
+		Audience:      app.JWTAudience,
+		Secret:        app.JWTSecret,
+		TokenExpiry:   time.Minute * 15,
+		RefreshExpiry: time.Hour * 24,
+		CookiePath:    "/",
+		CookieName:    "__Host-refresh_token",
+		CookieDomain:  app.CookieDomain,
+	}
 
 	// http.HandleFunc("/", Hello)
 	// http.HandleFunc("/about", About)
